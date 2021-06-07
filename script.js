87f0d8f1ae15f6ce446ced29096956771b19a004
Glitch (glittery-fresh-aardwolf) <none>
2021-06-07 14:52:58 +0000

    🛀👣 Checkpoint ./index.html:32265697/114 ./script.js:32265697/9

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function startQuiz(){
-  var score = 0
-  var answer = prompt("What is the 4 bit binary equivalent of 8?")
+  var score = 0;
+  var answer = prompt("What is the 4 bit binary equivalent of 8?");
   if(answer=="1000"){
-    alert("correct")
-    score = score + 1
+    alert("correct");
+    score = score + 1;
   }else{
-    alert("Wrong it was 1000")
+    alert("Wrong it was 1000");
   }
 }
