a48800e0abef58c5247816a259cd63cd9a078909
Glitch (glittery-fresh-aardwolf) <none>
2021-06-15 10:27:14 +0000

    😇🐏 Checkpoint ./script.js:22762240/1132 ./8ball.html:22762240/10659 ./index.html:22762240/5

--- script.js
+++ script.js
@@ -8,10 +8,29 @@ function startQuiz(){
     alert("Wrong it was 1000");
   }
 }
-  var answer = prompt("What is the 4 bit equivilent to 1 ");
-  if(answer=="1000"){
-    alert("correct");
-    score = score + 1;
-  }else{
-    alert("Wrong it was 0001");
+
+
+function chooseTask() {
+  var tasks = [
+    "Ask again later."
+"Concentrate and ask again.""
+"Don’t count on it.""
+"Most likely.""
+"My sources say no.""
+"Outlook not so good.""
+"Outlook good.""
+"Reply hazy, try again.""
+Very doubtful.
+Yes – definitely.
+  ];
+  //document.getElementById("msg").innerHTML = tasks[0];
+  document.getElementById("msg").innerHTML = ""; //blank out box
+  for (var i = 0; i < 6; i++) {
+    document.getElementById("msg").innerHTML =
+      document.getElementById("msg").innerHTML + "<br>" + tasks[i];
   }
+
+  var rand = Math.random() * tasks.length;
+  rand = Math.floor(rand);
+  document.getElementById("msg2").innerHTML = tasks[rand];
+}
