30e6b87716cba0447d1cf3a4685d70d703f572b3
Glitch (glittery-fresh-aardwolf) <none>
2021-06-15 17:36:51 +0000

    🏛📉 Checkpoint ./script.js:6829469/1488 ./index.html:6829469/31

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-function startQuiz(){
+function startQuiz() {
   var score = 0;
   var answer = prompt("What is the 4 bit binary equivalent of 8?");
-  if(answer=="1000"){
+  if (answer == "1000") {
     alert("correct");
     score = score + 1;
-  }else{
+  } else {
     alert("Wrong it was 1000");
   }
 }
 
-
 function ball() {
   var answers = [
     "Ask again later.",
-"Concentrate and ask again.",
-"Don’t count on it.",
-"Most likely.",
-"My sources say no.",
-"Outlook not so good.",
-"Outlook good.",
-"Reply hazy, try again.",
-"Very doubtful.",
-"Yes – definitely."
+    "Concentrate and ask again.",
+    "Don’t count on it.",
+    "Most likely.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Outlook good.",
+    "Reply hazy, try again.",
+    "Very doubtful.",
+    "Yes – definitely."
   ];
-  document.getElementById("msg").innerHTML = ""; 
-  for (var i = 0; i < 9; i++) 
-  {
-
-  var rand = Math.random() * tasks.length;
-  rand = Math.floor(rand);
-  document.getElementById("8ball").innerHTML = answers[rand]; }
+  document.getElementById("msg").innerHTML = "";
+  for (var i = 0; i < 9; i++) {
+    var rand = Math.random() * answers.length;
+    rand = Math.floor(rand);
+    document.getElementById("msg").innerHTML = answers[rand];
+  }
+}
