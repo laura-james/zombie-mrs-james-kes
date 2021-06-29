9e2215658ea8486697902ab6989f63acc6790d4f
Glitch (glittery-fresh-aardwolf) <none>
2021-06-29 20:27:49 +0000

    ✊🚙 Checkpoint ./script.js:6829469/11999 ./.glitch-assets:6829469/719

--- script.js
+++ script.js
@@ -1,47 +1,56 @@
 var zombie_map;
-var all_markers = []
-var data = `51.387156546966665 -2.343340865629464 https://cdn.glitch.com/1bf28fcc-9c66-4df1-b451-dfe5f696fac7%2Fzombie.png?v=1623920720709
-script.js:22 51.387133113126175 -2.345062843817025 https://cdn.glitch.com/1bf28fcc-9c66-4df1-b451-dfe5f696fac7%2Fzombie.png?v=1623920720709
-script.js:22 51.38641335360857 -2.345738760488778 https://cdn.glitch.com/1bf28fcc-9c66-4df1-b451-dfe5f696fac7%2Fzombie.png?v=1623920720709
-script.js:22 51.3859346235674 -2.343587628858834 https://cdn.glitch.com/1bf28fcc-9c66-4df1-b451-dfe5f696fac7%2Fzombie.png?v=1623920720709
-script.js:22 51.386195749665234 -2.3426756777937707 https://cdn.glitch.com/1bf28fcc-9c66-4df1-b451-dfe5f696fac7%2Fzombie.png?v=1623920720709
-script.js:22 51.38574379970742 -2.345717302816659 https://cdn.glitch.com/1bf28fcc-9c66-4df1-b451-dfe5f696fac7%2Fzombie.png?v=1623920720709
-script.js:22 51.38726702062457 -2.342267982023507 https://cdn.glitch.com/1bf28fcc-9c66-4df1-b451-dfe5f696fac7%2Fzombie.png?v=1623920720709
-script.js:22 51.38634974640908 -2.3432335772688684 https://cdn.glitch.com/1bf28fcc-9c66-4df1-b451-dfe5f696fac7%2Fzombie.png?v=1623920720709
-script.js:22 51.38653052453408 -2.3429063477690515 https://cdn.glitch.com/1bf28fcc-9c66-4df1-b451-dfe5f696fac7%2Fzombie.png?v=1623920720709
-script.js:22 51.38666443379585 -2.3430619158919153 https://cdn.glitch.com/1bf28fcc-9c66-4df1-b451-dfe5f696fac7%2Fzombie.png?v=1623920720709`
+var all_markers = [];
+var data = `51.387156546966665 -2.343340865629464 https://cdn.glitch.com/1bf28fcc-9c66-4df1-b451-dfe5f696fac7%2Fhospital.png
+51.387133113126175 -2.345062843817025 https://cdn.glitch.com/1bf28fcc-9c66-4df1-b451-dfe5f696fac7%2Fzombie.png
+51.38641335360857 -2.345738760488778 https://cdn.glitch.com/1bf28fcc-9c66-4df1-b451-dfe5f696fac7%2Fhospital.png
+51.3859346235674 -2.343587628858834 https://cdn.glitch.com/1bf28fcc-9c66-4df1-b451-dfe5f696fac7%2Fzombie.png
+51.386195749665234 -2.3426756777937707 https://cdn.glitch.com/1bf28fcc-9c66-4df1-b451-dfe5f696fac7%2Fzombie.png
+51.38574379970742 -2.345717302816659 https://cdn.glitch.com/1bf28fcc-9c66-4df1-b451-dfe5f696fac7%2Fzombie.png
+51.38726702062457 -2.342267982023507 https://cdn.glitch.com/1bf28fcc-9c66-4df1-b451-dfe5f696fac7%2Fzombie.png
+51.38634974640908 -2.3432335772688684 https://cdn.glitch.com/1bf28fcc-9c66-4df1-b451-dfe5f696fac7%2Fzombie.png
+51.38653052453408 -2.3429063477690515 https://cdn.glitch.com/1bf28fcc-9c66-4df1-b451-dfe5f696fac7%2Fzombie.png
+51.38666443379585 -2.3430619158919153 https://cdn.glitch.com/1bf28fcc-9c66-4df1-b451-dfe5f696fac7%2Fexplosion.gif`;
 
 var markers = data.split("\n");
-for(var i=0; i < markers.length; i++){
-    console.log(markers[i] + "<br>");
-  var marker_data = markers[i].trim();
-  marker_data = marker_data.split(" ");
-  var latitude = marker_data[0];
-var longitude = marker_data[1;
-var emoji = marker_data[2];
 
-var marker_position = new google.maps.LatLng(###, ###);
+function initMap() {
+  zombie_map = new google.maps.Map(document.getElementById("zombie_map"), {
+    zoom: 18,
+    center: { lat: 51.386634, lng: -2.344206 }
+  });
+  if(navigator.geolocation) {
+    navigator.geolocation.watchPosition(set_my_position);
 }
+else {
+    alert("Geolocation doesn't work in your browser");
+}
+  for (var i = 0; i < markers.length; i++) {
+    console.log(markers[i] + "<br>");
+    var marker_data = markers[i].trim();
+    marker_data = marker_data.split(" ");
+    var latitude = marker_data[0];
+    var longitude = marker_data[1];
+    var emoji = marker_data[2];
 
-      function initMap() {
-        zombie_map = new google.maps.Map(
-          document.getElementById("zombie_map"),
-          {
-            zoom: 18,
-            center: { lat: 51.386634, lng: -2.344206 }
-          }
-        );
-        zombie_map.addListener("click", function(e) {
-           var location = e.latLng;
-          placemarker(location);
-        });
-      }
-      function placemarker(location) {
-        var emoji = document.getElementById('icon_to_use').value;
-        var marker = new google.maps.Marker({
-          position: location,
-          map: zombie_map,
-          icon: emoji
-        });
-        console.log(location.lat() +" "+location.lng() +" "+emoji)
-      }
+    var marker_position = new google.maps.LatLng(latitude, longitude);
+    var marker = new google.maps.Marker({
+      position: marker_position,
+      map: zombie_map,
+      icon: emoji
+    });
+    all_markers.push(marker);
+  }
+  zombie_map.addListener("click", function(e) {
+    var location = e.latLng;
+    placemarker(location);
+  });
+}
+function placemarker(location) {
+  var emoji = document.getElementById("icon_to_use").value;
+  var marker = new google.maps.Marker({
+    position: location,
+    map: zombie_map,
+    icon: emoji
+  });
+  console.log(location.lat() + " " + location.lng() + " " + emoji);
+}
